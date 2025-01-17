151137f7af3e07bae7c6738aee8a7f91b9631adc
Muhammad Ali <muhammadali.karami@gmail.com>
2025-01-17 20:30:35 +0330

    added libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -56,4 +58,20 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // LiveData & ViewModel
+    implementation(libs.viewmodel.ktx)
+    implementation(libs.livedata.ktx)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines)
+
+    // Retrofit & Gson & Okhttp
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+    // DI (Hilt)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 }
