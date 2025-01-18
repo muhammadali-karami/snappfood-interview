d51ffef33e992e06951643e090d8ab9e955404e7
Muhammad Ali <muhammadali.karami@gmail.com>
2025-01-18 21:54:32 +0330

    added onBackPressedDispatcher to MainActivity

--- app/src/main/java/com/snappfood/interview/presentation/view/MainActivity.kt
+++ app/src/main/java/com/snappfood/interview/presentation/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.snappfood.interview.presentation.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import com.snappfood.interview.ui.theme.SnappFoodInterviewTheme
@@ -17,6 +18,10 @@ class MainActivity : ComponentActivity() {
                     AppNavigation()
                 }
             }
+        }
+
+        onBackPressedDispatcher.addCallback(this) {
+            finish()
         }
     }
 }
